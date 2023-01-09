b7a0ebfc9c42f102ceb6ad52846533cae43e6705
Summer <cmlee@gabia.com>
2023-01-09 09:39:14 +0900

    project init

---
+++ src/main/java/gabia/gvote/GVoteApplication.java
@@ -0,0 +1,13 @@
+package gabia.gvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GVoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gabia/gvote/GVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package gabia.gvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
